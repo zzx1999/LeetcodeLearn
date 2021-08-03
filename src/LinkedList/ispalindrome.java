664bf63c50e649b6825641bc12560d7d295270c2
zzx1999 <864387441@qq.com>
2021-08-03 19:41:48 +0800

    判断回文单链表，优化空间复杂度

--- src/LinkedList/ispalindrome.java
+++ src/LinkedList/ispalindrome.java
@@ -1,7 +1,51 @@
 package LinkedList;
 
 public class ispalindrome {
+    ListNode left;
     public boolean isPalindrome(ListNode head) {
+        //后续遍历
+//        left = head;
+//        return traverse(head);
 
+        //优化空间复杂度
+        left = head;
+        ListNode slow = head,fast = head;
+        //使用快慢指针找中点,slow走一步时fast走两步
+        while (fast!=null&&fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        //slow指向链表中点
+        if(fast!=null)
+            slow = slow.next;
+        ListNode right = reverse(slow);
+        while (right!=null){
+            if(left.val!=right.val)
+                return false;
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+
+    }
+    public boolean traverse(ListNode right){
+        if(right==null) return true;
+        boolean res = traverse(right.next);
+        res = res && (left.val==right.val);
+        left = left.next;
+        return res;
+    }
+    //优化空间复杂度
+    public ListNode reverse(ListNode head){
+        ListNode cur,pre,nxt;
+        cur = head;
+        pre = null;
+        while (cur!=null){
+            nxt = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = nxt;
+        }
+        return pre;
     }
 }
