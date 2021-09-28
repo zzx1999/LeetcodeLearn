92270f0226faf059c3431d761abf007a5ba6870e
zzx1999 <864387441@qq.com>
2021-09-28 10:25:34 +0800

    剑指offer 14/31 off13:机器人运动范围，BFS

--- src/backtrack/movingCountoff13.java
+++ src/backtrack/movingCountoff13.java
@@ -1,10 +1,16 @@
 package backtrack;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class movingCountoff13 {
     public int movingCount(int m, int n, int k) {
-        int[][] board = new int[m][n];
-        return dfs(board,0,0,m,n,k);
-//        return res;
+        //dfs
+//        int[][] board = new int[m][n];
+//        return dfs(board,0,0,m,n,k);
+        //bfs
+        return bfs(m,n,k);
+
     }
     public int dfs(int[][] board,int i,int j,int m,int n,int k){
         if(i>=m||i<0||j>=n||j<0||checkijk(i,j,k)||board[i][j]==1) return 0;//这个格子不能踩
@@ -15,6 +21,22 @@ public class movingCountoff13 {
         return 1 + dfs(board,i+1,j,m,n,k)+dfs(board,i,j+1,m,n,k);//1+向右可达总数+向下可达总数
 
 
+    }
+    public int bfs(int m,int n,int k){
+        boolean[][] visited = new boolean[m][n];
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{0,0});//加入起点0,0
+        int res = 0;
+        while (!q.isEmpty()){
+            int[] cell = q.poll();
+            if(cell[0]>=m||cell[0]<0||cell[1]>=n||cell[1]<0||checkijk(cell[0],cell[1],k)||visited[cell[0]][cell[1]])
+                continue;//这个单元格不满足条件的情况
+            visited[cell[0]][cell[1]] = true;
+            res++;
+            q.offer(new int[]{cell[0]+1,cell[1]});
+            q.offer(new int[]{cell[0],cell[1]+1});
+        }
+        return res;
     }
 
     private boolean checkijk(int i, int j, int k) {
