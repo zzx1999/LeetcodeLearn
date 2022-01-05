b7bf356b9544b539a83414b3df2e86899a7ac758
zzx1999 <864387441@qq.com>
2022-01-05 11:05:37 +0800

    剑指offer2 Queue 1. 向下路径节点之和

---
+++ src/Stackandqueue/pathSumoff050.java
@@ -0,0 +1,28 @@
+package Stackandqueue;
+
+import backtrack_off.TreeNode;
+
+public class pathSumoff050 {
+    public int pathSum(TreeNode root, int targetSum) {
+        if(root==null)
+            return 0;
+        int sum = rootSum(root,targetSum)+pathSum(root.left,targetSum)+pathSum(root.right,targetSum);
+        return sum;
+
+//        else{
+//            return pathSum(root.left,targetSum-root.val) + pathSum(root.left,targetSum)+pathSum(root.right,targetSum-root.val)+pathSum(root.right,targetSum);
+//        }
+    }
+    public int rootSum(TreeNode root,int val){//包含该节点的路径和为val的路径数目
+        if(root==null)
+            return 0;
+        int sum = 0;
+        if(root.val == val){
+            sum++;
+        }
+        sum += rootSum(root.left,val-root.val);
+        sum += rootSum(root.right,val-root.val);
+        return sum;
+
+    }
+}
