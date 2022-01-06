183d4e23197de34e8d3d52b7ab9c49d163259b7d
zzx1999 <864387441@qq.com>
2022-01-06 11:00:42 +0800

    剑指offer2 树(dfs) 1. 节点之和最大路径（dfs+动态规划，后序遍历）

---
+++ src/Stackandqueue/maxPathSumoff051.java
@@ -0,0 +1,21 @@
+package Stackandqueue;
+
+import backtrack_off.TreeNode;
+
+public class maxPathSumoff051 {
+    int maxres = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        maxRootSum(root);
+        return maxres;
+    }
+    public int maxRootSum(TreeNode root){//包含该节点的最大路径和
+        if(root==null)
+            return 0;
+        int lmax = Math.max(0,maxRootSum(root.left));
+        int rmax = Math.max(0,maxRootSum(root.right));
+        maxres = Math.max(root.val+lmax+rmax,maxres);
+        return root.val + Math.max(lmax,rmax);
+
+
+    }
+}
